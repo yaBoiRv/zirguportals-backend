8e75440216ef9a4a8d79680d8887fa18e93d5cd8
yaBoiRv <raimunds.vanags@gmail.com>
2026-02-08 21:52:50 +0200

    server

--- src/server.js
+++ src/server.js
@@ -48,7 +48,7 @@ const prisma = new PrismaClient({
 fastify.decorate('prisma', prisma);
 
 fastify.get('/version', async () => {
-  return { version: '1.0.3-fixed', timestamp: new Date().toISOString() };
+  return { version: '1.0.5-optional-schema', timestamp: new Date().toISOString() };
 });
 
 
